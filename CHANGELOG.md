83b153aac2a92c0a20e06a9ff867b0e0aa9fc25d
Dumitru Deveatii <dumitru@labs42.io>
2019-12-24 15:08:24 +0200

    feat: v2

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+
+## v2.0.0
+
+### Breaking changes
+
+- Dropped support of older Node.js versions. Officially supports Node.js version 8 and above.  
+
+- Removed exported `Operators` object. Use instead operators on the root.
+
+- Removed support for array `$pushAll` operator as it was deprecated by mongo since version 2.4.
+
+## v1.2.0
+
+### Features
+
+- Added support for `array` operators.
+
+- Added support for `bitwise` operators.
--- test/flatten.js
+++ test/flatten.js
@@ -1,49 +1,48 @@
 'use strict';
 
-var should = require('chai').should();
-var $ = require('../index');
-var mongo = require('mongodb');
+const $ = require('../index');
+const mongo = require('mongodb');
 
-describe('# Flatten tests', function(){
-  describe('# Primitive types', function () {
+describe('# Flatten tests', () => {
+  describe('# Primitive types', () => {
 
-    it('When is an empty object returns empty', function () {
+    it('When is an empty object returns empty', () => {
       $.flatten({}).should.deep.equal({});
     });
 
-    it('When is number returns the number', function () {
-      var n = 123;
+    it('When is number returns the number', () => {
+      const n = 123;
       $.flatten(n).should.equal(n);
     });
 
-    it('When is string returns the string', function () {
-      var s = 'abc';
+    it('When is string returns the string', () => {
+      const s = 'abc';
       $.flatten(s).should.equal(s);
     });
 
-    it('When is boolean returns the boolean', function () {
-      var b = false;
+    it('When is boolean returns the boolean', () => {
+      const b = false;
       $.flatten(b).should.equal(b);
     });
 
-    it('when is Date returns the Date', function () {
-      var date = new Date();
+    it('when is Date returns the Date', () => {
+      const date = new Date();
       $.flatten(date).should.equal(date);
     });
 
-    it('When is Array returns the array', function () {
-      var arr = [1, 2, 3];
+    it('When is Array returns the array', () => {
+      const arr = [1, 2, 3];
       $.flatten(arr).should.equal(arr);
     });
 
-    it('When is Array of mixed complex objects returns the array', function () {
-      var arr = [1, true, 'A', {x: 10, y: 20}];
+    it('When is Array of mixed complex objects returns the array', () => {
+      const arr = [1, true, 'A', {x: 10, y: 20}];
       $.flatten(arr).should.deep.equal(arr);
     });
   });
 
-  describe('# MongoDB types', function(){
-    var supportedTypes = [
+  describe('# MongoDB types', () => {
+    const supportedTypes = [
       'Binary',
       'Code',
       'DBRef',
@@ -59,15 +58,15 @@ describe('# Flatten tests', function(){
       'Timestamp'
     ];
 
-    supportedTypes.forEach(function(mongoType){
-      describe(mongoType, function(){
-        it('Flatten directly', function(){
-          var id = new mongo[mongoType]();
+    supportedTypes.forEach((mongoType) => {
+      describe(mongoType, () => {
+        it('Flatten directly', () => {
+          const id = new mongo[mongoType]();
           $.flatten(id).should.equal(id);
         });
 
-        it('Flatten as nested property', function(){
-          var data = {field: new mongo[mongoType]()};
+        it('Flatten as nested property', () => {
+          const data = {field: new mongo[mongoType]()};
           $.flatten(data).should.have
             .property('$set').that.have
             .property('field').that.equals(data.field);
@@ -76,132 +75,132 @@ describe('# Flatten tests', function(){
     });
   });
 
-  describe('# Simple objects', function () {
-    it('When has `a` string property sets the property', function () {
-      var obj = { a: 'test' };
+  describe('# Simple objects', () => {
+    it('When has `a` string property sets the property', () => {
+      const obj = { a: 'test' };
       $.flatten(obj).should.have.property('$set')
         .that.deep.equals(obj);
     });
 
-    it('When has `a` array property sets the property', function () {
-      var obj = { a: [1, 2, 3] };
+    it('When has `a` array property sets the property', () => {
+      const obj = { a: [1, 2, 3] };
       $.flatten(obj).should.have.property('$set')
         .that.deep.equals(obj);
     });
 
-    it('When has many properties sets the properties', function () {
-      var obj = { a: [1, 2, 3], b: 'test', c: {} };
+    it('When has many properties sets the properties', () => {
+      const obj = { a: [1, 2, 3], b: 'test', c: {} };
       $.flatten(obj).should.have.property('$set')
         .that.deep.equals(obj);
     });
     
-    it('When constructor is undefined sets object', function () {
-      var obj = { value: 'test' };
+    it('When constructor is undefined sets object', () => {
+      const obj = { value: 'test' };
       obj.constructor = undefined;
       $.flatten(obj).should.have.property('$set')
         .that.deep.equals(obj);
     });
   });
 
-  describe('# Nested operators ($ positional operator)', function () {
-    it('When positional operator with $set', function () {
-      var obj = { points: $.$().$set(10) };
+  describe('# Nested operators ($ positional operator)', () => {
+    it('When positional operator with $set', () => {
+      const obj = { points: $.$().$set(10) };
       $.flatten(obj).should.deep.equal({ $set: { 'points.$': 10 } });
     });
     
-    it('When positional operator with value', function () {
-      var obj = { points: $.$().value('test') };
+    it('When positional operator with value', () => {
+      const obj = { points: $.$().value('test') };
       $.flatten(obj).should.deep.equal({ $set: { 'points.$': 'test' } });
     });
     
-    it('When positional operator with $inc', function () {
-      var obj = { points: $.$().$inc(-1) };
+    it('When positional operator with $inc', () => {
+      const obj = { points: $.$().$inc(-1) };
       $.flatten(obj).should.deep.equal({ $inc: { 'points.$': -1 } });
     });
     
-    it('When positional operator with field specified', function () {
-      var obj = { points: $.$('std').$set(0.5) };
+    it('When positional operator with field specified', () => {
+      const obj = { points: $.$('std').$set(0.5) };
       $.flatten(obj).should.deep.equal({ $set: { 'points.$.std': 0.5 } });
     });
     
-    it('When positional operator with field specified in nested object', function () {
-      var obj = {stats: { group: { points: $.$('std').$set(0.5) }}};
+    it('When positional operator with field specified in nested object', () => {
+      const obj = {stats: { group: { points: $.$('std').$set(0.5) }}};
       $.flatten(obj).should.deep.equal({ $set: { 'stats.group.points.$.std': 0.5 } });
     });
   });
   
-  describe('# Empty values', function() {
+  describe('# Empty values', () =>  {
 
-    it('When property value is null sets to null', function () {
-      var obj = { x: null };
+    it('When property value is null sets to null', () => {
+      const obj = { x: null };
       $.flatten(obj).should.deep.equal({ $set: { x: null } });
     });
 
-    it('When property value is undefined sets to undefined', function () {
-      var obj = { x: undefined };
+    it('When property value is undefined sets to undefined', () => {
+      const obj = { x: undefined };
       $.flatten(obj).should.deep.equal({ $set: { x: undefined } });
     });
 
   });
 
-  describe('# Simple objects using operators', function () {
+  describe('# Simple objects using operators', () => {
 
-    it('When a property uses $inc', function () {
-      var obj = { x: 1, y: $.$inc(2) };
+    it('When a property uses $inc', () => {
+      const obj = { x: 1, y: $.$inc(2) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $inc: { y: 2 } });
     });
 
-    it('When more properties use $inc', function () {
-      var obj = { x: 1, y: $.$inc(), z: $.$inc(10) };
+    it('When more properties use $inc', () => {
+      const obj = { x: 1, y: $.$inc(), z: $.$inc(10) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $inc: { y: 1, z: 10 } });
     });
 
-    it('When a property uses $mul', function () {
-      var obj = { x: 1, y: $.$mul(2) };
+    it('When a property uses $mul', () => {
+      const obj = { x: 1, y: $.$mul(2) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $mul: { y: 2 } });
     });
 
-    it('When more properties use $mul', function () {
-      var obj = { x: 1, y: $.$mul(), z: $.$mul(10) };
+    it('When more properties use $mul', () => {
+      const obj = { x: 1, y: $.$mul(), z: $.$mul(10) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $mul: { y: 1, z: 10 } });
     });
 
-    it('When a property uses $rename', function () {
-      var obj = { x: 1, y: $.$rename(2) };
+    it('When a property uses $rename', () => {
+      const obj = { x: 1, y: $.$rename(2) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $rename: { y: 2 } });
     });
 
-    it('When more properties use $rename', function () {
-      var obj = { x: 1, y: $.$rename('alias 1'), z: $.$rename('alias 2') };
+    it('When more properties use $rename', () => {
+      const obj = { x: 1, y: $.$rename('alias 1'), z: $.$rename('alias 2') };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $rename: { y: 'alias 1', z: 'alias 2' } });
     });
 
-    it('When a property uses $setOnInsert', function () {
-      var obj = { x: 1, y: $.$setOnInsert(2) };
+    it('When a property uses $setOnInsert', () => {
+      const obj = { x: 1, y: $.$setOnInsert(2) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $setOnInsert: { y: 2 } });
     });
 
-    it('When more properties use $setOnInsert', function () {
-      var obj = { x: 1, y: $.$setOnInsert([1, 2]), z: $.$setOnInsert(3) };
+    it('When more properties use $setOnInsert', () => {
+      const obj = { x: 1, y: $.$setOnInsert([1, 2]), z: $.$setOnInsert(3) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $setOnInsert: { y: [1, 2], z: 3 } });
     });
 
-    it('When a property uses $set', function () {
-      var obj = { x: 1, y: $.$set(2) };
+    it('When a property uses $set', () => {
+      const obj = { x: 1, y: $.$set(2) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1, y: 2 } });
     });
 
-    it('When more properties use $set', function () {
-      var obj = {
+    it('When more properties use $set', () => {
+      const obj = {
         x: 1,
         y: $.$set([1, 2, 3]),
         z: $.$set({ inner: 'object', a: { b: 'c' } })
@@ -211,74 +210,74 @@ describe('# Flatten tests', function(){
         .equal({ $set: { x: 1, y: [1, 2, 3], z: { inner: 'object', a: { b: 'c' } } } });
     });
 
-    it('When a property uses $unset', function () {
-      var obj = { x: 1, y: $.$unset() };
+    it('When a property uses $unset', () => {
+      const obj = { x: 1, y: $.$unset() };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $unset: { y: '' } });
     });
 
-    it('When more properties use $unset', function () {
-      var obj = { x: 1, y: $.$unset(), z: $.$unset() };
+    it('When more properties use $unset', () => {
+      const obj = { x: 1, y: $.$unset(), z: $.$unset() };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $unset: { y: '', z: '' } });
     });
 
-    it('When a property uses $min', function () {
-      var obj = { x: 1, y: $.$min(10) };
+    it('When a property uses $min', () => {
+      const obj = { x: 1, y: $.$min(10) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $min: { y: 10 } });
     });
 
-    it('When more properties use $min', function () {
-      var obj = { x: 1, y: $.$min(-1), z: $.$min('abc') };
+    it('When more properties use $min', () => {
+      const obj = { x: 1, y: $.$min(-1), z: $.$min('abc') };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $min: { y: -1, z: 'abc' } });
     });
 
-    it('when a property uses $max', function () {
-      var obj = { x: 1, y: $.$max(10) };
+    it('when a property uses $max', () => {
+      const obj = { x: 1, y: $.$max(10) };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $max: { y: 10 } });
     });
 
-    it('when more properties use $max', function () {
-      var obj = { x: 1, y: $.$max(-1), z: $.$max('abc') };
+    it('when more properties use $max', () => {
+      const obj = { x: 1, y: $.$max(-1), z: $.$max('abc') };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $max: { y: -1, z: 'abc' } });
     });
 
-    it('When a property uses $currentDate', function () {
-      var obj = { x: 1, y: $.$currentDate() };
+    it('When a property uses $currentDate', () => {
+      const obj = { x: 1, y: $.$currentDate() };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $currentDate: { y: { $type: 'date' } } });
     });
 
-    it('When more properties use $max', function () {
-      var obj = { x: 1, y: $.$currentDate(), z: $.$currentDate() };
+    it('When more properties use $max', () => {
+      const obj = { x: 1, y: $.$currentDate(), z: $.$currentDate() };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $currentDate: { y: {$type: 'date'}, z: {$type: 'date'} } });
     });
 
-    it('When a property uses $timestamp', function () {
-      var obj = { x: 1, y: $.$timestamp() };
+    it('When a property uses $timestamp', () => {
+      const obj = { x: 1, y: $.$timestamp() };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $currentDate: { y: { $type: 'timestamp' } } });
     });
 
-    it('When more properties use $timestamp', function () {
-      var obj = { x: 1, y: $.$timestamp(), z: $.$timestamp() };
+    it('When more properties use $timestamp', () => {
+      const obj = { x: 1, y: $.$timestamp(), z: $.$timestamp() };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1 }, $currentDate: { y: { $type: 'timestamp' }, z: { $type: 'timestamp' } } });
     });
 
-    it('When a property uses null', function () {
-      var obj = { x: 1, y: null };
+    it('When a property uses null', () => {
+      const obj = { x: 1, y: null };
       $.flatten(obj).should.deep
         .equal({ $set: { x: 1,  y: null } });
     });
 
-    it('When uses all field operators', function () {
-      var obj = {
+    it('When uses all field operators', () => {
+      const obj = {
         a1: 1, a2: 11,
         b1: $.$inc(2), b2: $.$inc(22),
         c1: $.$mul(3), c2: $.$mul(33),
@@ -292,7 +291,7 @@ describe('# Flatten tests', function(){
         k1: $.$timestamp(), k2: $.$timestamp()
       };
 
-      var expectedValue = {
+      const expectedValue = {
         $set: { a1: 1, a2: 11, f1: new Date(2000, 1, 1), f2: new Date(2016, 1, 1) },
         $inc: { b1: 2, b2: 22 },
         $mul: { c1: 3, c2: 33 },
@@ -307,19 +306,19 @@ describe('# Flatten tests', function(){
     });
   });
 
-  describe('# Nested objects', function () {
-    it('When has inner property', function () {
-      var obj = { a: {b: {c: 1}} };
+  describe('# Nested objects', () => {
+    it('When has inner property', () => {
+      const obj = { a: {b: {c: 1}} };
       $.flatten(obj).should.deep.equal({$set: {'a.b.c': 1}});
     });
 
-    it('When has inner property that is empty object', function () {
-      var obj = { a: {} };
+    it('When has inner property that is empty object', () => {
+      const obj = { a: {} };
       $.flatten(obj).should.deep.equal({$set: {'a': {}}});
     });
 
-    it('When has many inner properties', function () {
-      var obj = {
+    it('When has many inner properties', () => {
+      const obj = {
         a: {
           b: {
             c: 1,
@@ -334,7 +333,7 @@ describe('# Flatten tests', function(){
         x: 'test'
       };
 
-      var expectedValue = {
+      const expectedValue = {
         $set: {
           'a.b.c': 1,
           'a.b.d': 2,
@@ -346,9 +345,9 @@ describe('# Flatten tests', function(){
       $.flatten(obj).should.deep.equal(expectedValue);
     });
 
-    it('When has many inner properties with operators', function () {
-      var id = new mongo.ObjectID();
-      var obj = {
+    it('When has many inner properties with operators', () => {
+      const id = new mongo.ObjectID();
+      const obj = {
         id: id,
         a: {
           b: {
@@ -367,7 +366,7 @@ describe('# Flatten tests', function(){
         x: $.$rename('test')
       };
 
-      var expectedValue = {
+      const expectedValue = {
         $set: {
           'id': id,
           'a.b.d': 2,
